bc780f5c946c4cfec1b9afad8828b84ed3aff1bf
TPLuG9527 <2992454590@qq.com>
2022-10-27 00:27:12 +0800

    MAIN

--- task1/main.c
+++ task1/main.c
@@ -3,22 +3,21 @@
 int main(void)
 {
 	double h=0.001;
-	double a;
+	double a=2;
 	double H=(2-h)/(2+h);
 	int k;
 	int t= 0;
 	double x=0;
     printf("Please enter a number to decide a");
-    scanf("%lf",&a);
     double final1=a;
 	double final2=a*H;
 	printf("x          y\n");
-	for(k=a;(final1-final2)>0.0001;t++)
+	while((final1-final2)>=0.0001)
 	{
 	
 		x+=h;
-		final1=a*pow(H,t);
-		final2=a*pow(H,t+1);
+		final1=final1*H;
+		final2=final2*H;
 		printf("%lf  %.19lf\n",x,final1);
 	         
 	}
